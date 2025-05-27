09a092ab746001dff8cf11a0d501bf86949d57c9
Babapeer <69247426+Babapeer@users.noreply.github.com>
2025-05-27 10:15:17 +0530

    Update main.tf

--- eks-install/modules/vpc/main.tf
+++ eks-install/modules/vpc/main.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
     Name                                           = "${var.cluster_name}-private-${count.index + 1}"
