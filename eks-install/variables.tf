bd06ad43e70f7cfb42d3e9ecb653f7e273b5443b
Babapeer <69247426+Babapeer@users.noreply.github.com>
2025-05-31 09:57:17 +0530

    Update variables.tf

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-northeast-3"
 }
 
 variable "vpc_cidr" {
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["ap-northeast-3a", "ap-northeast-3b", "ap-northeast-3c"]
 }
 
 variable "private_subnet_cidrs" {
@@ -53,7 +53,7 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.micro"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 1
